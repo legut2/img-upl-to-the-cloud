c5149864092370cc00a01e8fc7aa1da5eeb4265f
Daniel Legut <legut2@illinois.edu>
2020-06-20 14:56:18 -0500

    added basic base64 logic

--- src/upload/handler.py
+++ src/upload/handler.py
@@ -3,4 +3,10 @@ def handler(event, context):
     # Log the event argument for debugging and for use in local development.
     print(json.dumps(event))
 
-    return {}
+    try:
+        if event['isBase64Encoded']:
+            return "You uploaded a base64encoded thing to this function"
+        else:
+            return "You didn't base64encode anything to this funciton"
+    catch:
+        return "Something went terribly wrong."
