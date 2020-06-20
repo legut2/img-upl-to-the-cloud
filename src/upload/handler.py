bd45718fd66fdddb139fad427aa477497448d607
Daniel Legut <legut2@illinois.edu>
2020-06-20 17:34:10 -0500

    uploaded upload to s3 bucket part

--- src/upload/handler.py
+++ src/upload/handler.py
@@ -11,12 +11,19 @@ def handler(event, context):
             img_data = event['body'].encode()
             m = hashlib.md5()
             m.update(img_data)
-
-            with open("/tmp/" + m.hexdigest() + ".jpeg", "wb") as fh:
+            imageName = m.hexdigest() + ".jpeg"
+            imagePath = "/tmp/" + imageName
+            with open(imagePath, "wb") as fh:
                 fh.write(base64.decodebytes(img_data))
             
-
-            return "You uploaded a base64encoded thing to this function and wrote a jpeg but never checked to make sure it was a jpeg"
+            print("Storing thumbnail %s to ObjectStore 'Uploaded Image'" % imagePath)
+            targetBucket = os.environ['BUCKET_NAME']
+            try:
+                s3.Bucket(targetBucket).upload_file(imagePath, imageName)
+            except botocore.exceptions.ClientError as e:
+                print("Error uploading %s to bucket %s" % (imageName, targetBucket))
+                raise
+            return "You uploaded a base64encoded thing to this function and wrote a jpeg but never checked to make sure it was a jpeg then tried to upload it to s3 bucket"
         else:
             return "You didn't base64encode anything to this funciton"
     except KeyError:
