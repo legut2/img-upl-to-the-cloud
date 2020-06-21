c7eba5a34555bf5308d79a42938366d92b7d2951
Daniel Legut <legut2@illinois.edu>
2020-06-21 18:25:03 -0500

    added some comments

--- src/upload/handler.py
+++ src/upload/handler.py
@@ -17,32 +17,35 @@ def handler(event, context):
 
     try:
         if event['isBase64Encoded']:
-            # b64_form_data = event['body'].encode()
-            # form_data = base64.decodebytes(b64_form_data)
-            body = event["body"]
 
-            content_type = event["headers"]["content-type"]
-
+            body = event["body"] # body that is base64 encoded
+            content_type = event["headers"]["content-type"] # what kind of content type it is, presumably it is multipart dataform
+            # decode base  64
             body_dec = base64.b64decode(body)
 
+            #decode form-data from multipart form
+            #multipart forms are very common due to html forms
             multipart_data = decoder.MultipartDecoder(body_dec, content_type)
-
             binary_content = []
-
             for part in multipart_data.parts:
                 binary_content.append(part.content)
 
             imageStream = io.BytesIO(binary_content[0])
             imageFile = Image.open(imageStream)
             imageArray = np.array(imageFile)
-            # Create new image from the Numpy array and save
+
+            # Create new image from the Numpy array and save to sanitize the image to help prevent something malicious as payload
             result = Image.fromarray(imageArray)
-            # return str(form_data)
+
             m = hashlib.md5()
-            # m.update(img_data)
-            imageName = m.hexdigest() + ".jpg"
-            imagePath = "/tmp/" + imageName
+
+            # m.update(img_data) # Make hash specific to image
+
+            imageName = m.hexdigest() + ".jpg" # image name is now hash + jpg
+            imagePath = "/tmp/" + imageName # tmp is one of the few places where you can write a file, keep in mind container reuse is possible with lambdas.
             result.save(imagePath)
+
+            # Upload to s3 bucket finally that is in env variable
             s3 = boto3.resource('s3')
             print("Storing thumbnail %s to ObjectStore 'Uploaded Image'" % imagePath)
             targetBucket = os.environ['BUCKET_NAME']
